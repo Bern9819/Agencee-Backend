6b41af37582f5530c1c4fa7e3974d8680fb01e09
Bern9819 <bernardo.cavallo.syncope@gmail.com>
2025-03-25 16:19:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { getAvailableCollaborators } = require(__dirname + '/calendar-utils');
-
+const { getAvailableCollaborators } = require ('./calendars-utils');
 
 const app = express();
 const port = process.env.PORT || 3000;
